7712c2388bd523ae5ab9c5cfab5bbe2fc06e05fd
BernhardKonrad <bernhard.konrad@gmail.com>
2014-02-22 14:30:30 -0800

    Actually, 40 is the right number. No need for a variable here.

--- 01-toyline.R
+++ 01-toyline.R
@@ -1,14 +1,14 @@
-n <- 401
+rm(list=ls())
 a <- 3
 b <- -6
 sigSq <- 0.5
-x <- runif(n)
-y <- a + b * x + rnorm(n, sd = sigSq)
+x <- runif(40)
+y <- a + b * x + rnorm(40, sd = sigSq)
 
 avgX <- mean(x)
 write(avgX, "avgX.txt")
 
 plot(x,y)
-abline(a, b, col="green")
+abline(a, b, col="red")
 dev.print(pdf, "toylinePlot.pdf")
  
